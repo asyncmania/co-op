d31873241d19fb06200ae5746c6b52c4c6d184c3
jilesanmi <ilesanmi.josiah@gmail.com>
2020-08-05 14:02:06 +0100

    db connection

--- config/database.php
+++ config/database.php
@@ -38,14 +38,15 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
+        
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'fmard_coop_db'),
+            'username' => env('DB_USERNAME', 'fmardcoop'),
+            'password' => env('DB_PASSWORD', 'fmardcoop001'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
